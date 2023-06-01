695a82edb9ff11b9b075e8aef38c6aa9cee10b2c
lucianobajr <luciano.alcantara@ufv.br>
2023-06-01 18:22:03 -0300

    barn

--- src/screens/main/widgets/barn.py
+++ src/screens/main/widgets/barn.py
@@ -1,40 +1,119 @@
-from PySide2.QtWidgets import QWidget, QSizePolicy,QApplication,QPushButton, QMainWindow, QVBoxLayout, QHBoxLayout, QLabel, QLineEdit, QFrame, QVBoxLayout, QSpacerItem
-from PySide2.QtGui import QPixmap, QIcon, QPainter, QColor
-import sys
+from PySide2.QtWidgets import QWidget, QVBoxLayout, QLabel, QScrollArea, QHBoxLayout, QPushButton, QVBoxLayout, QFrame, QLineEdit
+from PySide2.QtGui import QPixmap, QIcon
+from PySide2.QtCore import Qt,QSize
 
-class BarnWidget(QMainWindow):
-    # def __init__(self):
-    #     super().__init__()
+recipe_data = {
+    "id": "f57255d9-afc0-478e-949b-0301f0bc05d0",
+    "name": "teste em aberto",
+    "bio": None,
+    "photo": None,
+    "posts": "2 posts",
+    "following": "1 seguindo",
+    "followers": "2 seguidores",
+    "recipes": [
+        {
+            "id": "ab066eb8-0f05-434d-abad-fc1a8027d94f",
+            "name": "New",
+            "description": "new recipe",
+            "created_at": "22/05/2023"
+        },
+        {
+            "id": "84ee64dd-3c0f-4563-81b6-f77772965757",
+            "name": "tests datetime",
+            "description": "new recipe tests datetime",
+            "created_at": "20/05/2023"
+        },
+        {
+            "id": "ab066eb8-0f05-434d-abad-fc1a8027d94f",
+            "name": "New",
+            "description": "new recipe",
+            "created_at": "22/05/2023"
+        },
+        {
+            "id": "84ee64dd-3c0f-4563-81b6-f77772965757",
+            "name": "tests datetime",
+            "description": "new recipe tests datetime",
+            "created_at": "20/05/2023"
+        },
+        {
+            "id": "ab066eb8-0f05-434d-abad-fc1a8027d94f",
+            "name": "New",
+            "description": "new recipe",
+            "created_at": "22/05/2023"
+        },
+        {
+            "id": "84ee64dd-3c0f-4563-81b6-f77772965757",
+            "name": "tests datetime",
+            "description": "new recipe tests datetime",
+            "created_at": "20/05/2023"
+        },
+        {
+            "id": "ab066eb8-0f05-434d-abad-fc1a8027d94f",
+            "name": "New",
+            "description": "new recipe",
+            "created_at": "22/05/2023"
+        },
+        {
+            "id": "84ee64dd-3c0f-4563-81b6-f77772965757",
+            "name": "tests datetime",
+            "description": "new recipe tests datetime",
+            "created_at": "20/05/2023"
+        },
+        {
+            "id": "ab066eb8-0f05-434d-abad-fc1a8027d94f",
+            "name": "New",
+            "description": "new recipe",
+            "created_at": "22/05/2023"
+        },
+        {
+            "id": "84ee64dd-3c0f-4563-81b6-f77772965757",
+            "name": "tests datetime",
+            "description": "new recipe tests datetime",
+            "created_at": "20/05/2023"
+        },
+        {
+            "id": "ab066eb8-0f05-434d-abad-fc1a8027d94f",
+            "name": "New",
+            "description": "new recipe",
+            "created_at": "22/05/2023"
+        },
+        {
+            "id": "84ee64dd-3c0f-4563-81b6-f77772965757",
+            "name": "tests datetime",
+            "description": "new recipe tests datetime",
+            "created_at": "20/05/2023"
+        }
+    ]
+}
 
-    #     layout = QVBoxLayout()
-    #     self.setLayout(layout)
 
-    #     self.setSizePolicy(QSizePolicy.Expanding, QSizePolicy.Expanding)
-    #     layout.setContentsMargins(0, 0, 0, 0)
-    #     layout.setSpacing(0)
-
-    #     self.setStyleSheet("background-color: #FFFFFF;")
+class BarnWidget(QWidget):
     def __init__(self):
         super().__init__()
-        self.setWindowTitle("Armazém")
-        self.setStyleSheet("background-color: #FFFFFF;")
-        
-        # Título alinhado à esquerda na parte superior
+
+        # Create the main layout
+        layout = QVBoxLayout()
+        layout.setContentsMargins(48, 48, 48, 0)
+        layout.setSpacing(16)
+        self.setLayout(layout)
+
+        # Create the title label
         title_label = QLabel("Armazém")
         title_label.setStyleSheet(
-                                "font-family: 'Roboto Slab';"
-                                "font-style: normal;"
-                                "font-weight: 900;"
-                                "font-size: 32px;"
-                                # "line-height: 42px;"
-                                "color: #341A0F;"
-                                "margin: 0px 0px 0px 48px;"
-                                )
-        
-        # Barra de pesquisa
+            "font-family: 'Roboto Slab';"
+            "font-style: normal;"
+            "font-weight: 900;"
+            "font-size: 32px;"
+            "line-height: 42px;"
+            "color: #341A0F;"
+        )
+        layout.addWidget(title_label)
+
+        # Create a horizontal layout for the input and button
         input_layout = QHBoxLayout()
         input_layout.setSpacing(8)
-        
+
+        # Create the search input
         input_widget = QLineEdit()
         input_widget.setFixedWidth(496)
         input_widget.setFixedHeight(60)
@@ -42,7 +121,8 @@ class BarnWidget(QMainWindow):
             "background: #F2F2F2; border-radius: 20px; padding: 10px; font-size: 16px;"
         )
         input_widget.setPlaceholderText("Pesquisar")
-        
+        input_layout.addWidget(input_widget)
+
         # Create the search button
         search_button = QPushButton()
         # Replace with your search icon path
@@ -59,62 +139,99 @@ class BarnWidget(QMainWindow):
         search_button.clicked.connect(
             lambda: self.search(value=input_widget.text()))
         input_layout.addWidget(search_button)
-        
-        # Layout principal
-        main_layout = QVBoxLayout()
-        main_layout.setSpacing(16)
-        main_layout.setContentsMargins(0, 0, 0, 0)
-        main_layout.addWidget(title_label)
-        main_layout.addWidget(input_widget)
-        main_layout.addLayout(input_layout)
-        self.setLayout(main_layout)
-        
-        # Exemplo de box de receita
-        recipe_box_1 = RecipeBox("Nome da Receita 1", "data 1", "src/assets/images/canjiquinha.png")
-        recipe_box_2 = RecipeBox("Nome da Receita 2", "data 2", "caminho/foto2.jpg")
-        
-        # Organização dos boxes de receita em 2 colunas
-        recipe_layout = QHBoxLayout()
-        recipe_layout.addWidget(recipe_box_1)
-        recipe_layout.addWidget(recipe_box_2)
-        
-        # Adicionando o layout das receitas ao layout principal
-        main_layout.addLayout(recipe_layout)
-        
-        # Widget principal
-        central_widget = QWidget()
-        central_widget.setLayout(main_layout)
-        self.setCentralWidget(central_widget)
 
+        # Add the input and button layout to the main layout
+        layout.addLayout(input_layout)
 
+        # Create a scroll area for the recipe posts
+        scroll_area = QScrollArea()
+        scroll_area.setWidgetResizable(True)
+        scroll_area.setStyleSheet(
+            "QScrollArea { background-color: #FFFFFF; border: none; }"
+            "QScrollBar:vertical { background-color: #F2F2F2; width: 12px; margin: 0px; }"
+            "QScrollBar::handle:vertical { background-color: #C4C4C4; border-radius: 6px; min-height: 20px; }"
+            "QScrollBar::add-line:vertical, QScrollBar::sub-line:vertical { background: none; }"
+            "QScrollBar::add-page:vertical, QScrollBar::sub-page:vertical { background: none; }"
+        )
+        layout.addWidget(scroll_area)
 
+        # Create a widget to hold the recipe posts
+        feed_container = QWidget()
+        feed_container_layout = QVBoxLayout(feed_container)
+        feed_container_layout.setContentsMargins(0, 0, 0, 0)
+        feed_container.setLayout(feed_container_layout)
 
+        # Add recipe posts dynamically (replace with your own logic)
+        max_recipes_per_row = 3
+        row_layout = None
+    
+        for i, recipe in enumerate(recipe_data["recipes"]):
+            if i % max_recipes_per_row == 0:
+                row_layout = QHBoxLayout()
+                feed_container_layout.addLayout(row_layout)
 
-class RecipeBox(QFrame):
-    def __init__(self, name, date, photo_path):
-        super().__init__()
-        self.setFrameStyle(QFrame.Panel)
-        self.setStyleSheet("background-color: white; margin: 10px; padding: 10px;")
-        
-        # Nome da receita
-        name_label = QLabel(name)
-        name_label.setStyleSheet("font-weight: bold; font-size: 16px;")
-        
-        # Data de publicação
-        date_label = QLabel(date)
-        
-        # Foto do usuário
-        user_photo = QLabel()
-        user_photo.setPixmap(photo_path)  # Aqui é necessário ajustar o tamanho da foto
-        
-        # Foto da receita
-        recipe_photo = QLabel()
-        recipe_photo.setPixmap(photo_path)  # Aqui é necessário ajustar o tamanho da foto
-        
-        # Layout interno
-        internal_layout = QVBoxLayout()
-        internal_layout.addWidget(name_label)
-        internal_layout.addWidget(date_label)
-        internal_layout.addWidget(user_photo)
-        internal_layout.addWidget(recipe_photo)
-        self.setLayout(internal_layout)
+            # Create the post container
+            post_container = QFrame()
+            post_container.setStyleSheet(
+                "QFrame { background: #FFFFFF; border: 2px solid #F2F2F2; border-radius: 16px; padding: 11px 16px; }"
+            )
+            post_container.setFixedSize(200, 320)
+
+            layout = QVBoxLayout(post_container)
+            layout.setContentsMargins(0, 0, 0, 0)
+            layout.setSizeConstraint(QVBoxLayout.SetMinimumSize)  # Set the size constraint
+            layout.setAlignment(Qt.AlignCenter)  # Center-align the contents of the layout
+
+            # Create the recipe name label
+            recipe_name_label = QLabel(recipe["name"])
+            recipe_name_label.setStyleSheet(
+                "font-family: 'Roboto Slab';"
+                "font-style: normal;"
+                "font-weight: 700;"
+                "font-size: 18px;"
+                "line-height: 24px;"
+                "color: #341A0F;"
+                "border: none;"
+            )
+
+            # Create the text and save button container
+            text_container = QVBoxLayout()
+            text_container.setSpacing(8)
+            text_container.addWidget(recipe_name_label)
+
+            # Add the text container to the layout
+            layout.addLayout(text_container)
+
+            # Add the recipe photo (assuming you have the path to the image)
+            photo_path = "src/assets/images/recipe.png"
+            recipe_photo_label = QLabel()
+            recipe_photo_pixmap = QPixmap(photo_path)  # Replace with the actual path to the photo
+            recipe_photo_label.setFixedSize(192, 192)
+            recipe_photo_label.setPixmap(recipe_photo_pixmap.scaled(
+                recipe_photo_label.size(),
+                Qt.AspectRatioMode.IgnoreAspectRatio,
+                Qt.TransformationMode.SmoothTransformation
+            ))
+            recipe_photo_label.setStyleSheet("border:none;")  # Remove border styling from the photo label
+
+            # Add the recipe photo label to the layout
+            layout.addWidget(recipe_photo_label, alignment=Qt.AlignCenter)
+
+            # Create the profile image and save button container
+            profile_container = QHBoxLayout()
+            profile_container.setSpacing(8)
+
+            row_layout.addWidget(post_container)
+
+        scroll_area.setWidget(feed_container)
+
+    def search(self, value):
+        message = {
+            "topic": "@search/dive_and_users",
+            "body": {
+                "user_id": "75621072-e6b5-49ae-a5ff-424707d534b2",
+                "value": value
+            }
+        }
+
+        print(message)
